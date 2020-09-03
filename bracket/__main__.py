6362f8306ddfca7ca61a1694ab6b67d83fa512c5
henryli6 <henry8254@gmail.com>
2020-09-02 23:38:22 -0400

    removed print statements

--- bracket/__main__.py
+++ bracket/__main__.py
@@ -4,14 +4,11 @@ import sys
 if __name__ == '__main__':
     if(len(sys.argv) == 1):
         year = 19
-        print('asdfjkl;')
     else:
         try:
             year = int(sys.argv[1])
             assert year >= 15 and year <= 20
-            print('bruh')
         except:
             year = 19
-            print('dood')
 
     Bracket(width=1200, height=600, year=year).run()
--- bracket/model.py
+++ bracket/model.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-
 all_games_19 = pd.read_csv("bracket/data/all_games_19_training_data.csv")
 
 columns = []
@@ -14,6 +13,6 @@ features = all_games_19[columns]
 target = all_games_19["Away Win"]
 
 x_train, x_test, y_train, y_test = train_test_split(
-    features, target, test_size=0.3)
+    features, target, test_size=0.5)
 model = KNeighborsClassifier(n_neighbors=50)
 model.fit(x_train, y_train)
